c6d8228536a2c1f5e53c1da1c07011a7e4f38c69
michelle-currier <mushel@gmail.com>
2025-09-06 10:34:36 -0500

    some updates that lisa requested

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar, faPassport } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
@@ -19,26 +20,21 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full btn-green flex items-center justify-center gap-2 font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            className="rounded-full btn-green flex items-center justify-center gap-2 font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto md:w-[158px]"
             href="https://saintclaudeyogacenter.union.site/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Register for class
+            <FontAwesomeIcon icon={faCalendar} />
+            Schedule
           </a>
           <a
-            className="rounded-full btn-rust flex items-center justify-center font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            className="rounded-full btn-rust flex items-center justify-center gap-2 font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
             href="https://saintclaudeyogacenter.union.site/passes"
             target="_blank"
             rel="noopener noreferrer"
           >
+            <FontAwesomeIcon icon={faPassport} />
             Get Passes
           </a>
         </div>
