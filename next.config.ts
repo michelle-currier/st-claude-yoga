96246b3ce8bc93c3cac30d98d21366e54c7e41a4
michelle-currier <mushel@gmail.com>
2025-09-09 10:19:58 -0500

    added the redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config redirect delete lines 5-14 to remove redirect */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://saintclaudeyogacenter.union.site/', // Replace with your desired external URL
+        permanent: true,
+        basePath: false, // Set to false if not using basePath
+      },
+    ];
+  },
 };
 
 export default nextConfig;
